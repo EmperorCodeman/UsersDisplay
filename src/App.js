2dd6bbffd90a021d55921abfb9a20c4f43d112e1
Alex Morrison <odetocodeman@gmail.com>
2018-04-01 12:25:35 -0600

    april

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import {Carousel} from "react-bootstrap" 
+import {Carousel, Modal, Button} from "react-bootstrap" 
 import logo from './logo.svg';
 import './App.css';
 import initialUsers from './static/users.json'
 const axios = require("axios")
-
 
 class App extends Component {
   constructor(props, context) {
@@ -14,23 +13,32 @@ class App extends Component {
       users: initialUsers
     };
   }
-  handleClick = () =>{
-    console.log('hi')
+  
+  handleClose = () => {
+    this.setState({ show: false, user: {} });
   }
+
+  handleClick = (user) =>{
+    this.setState({ show: true, user: user })
+  }
+
   handleSelect = (key, event) => {
-    console.log("estdd", key, event)    
+    //console.log("estdd", key, event)    
+    //'https://randomuser.me/api/?results=3'
     axios.get('https://randomuser.me/api/?results=3')
     .then( (response) => {
       // concat new user array to existing array
-      this.setState({ users: this.state.users.concat(response.data.results)})
-    console.log(response.data.results[0]);
-    return response.data.results
+      this.setState({ users: response.data.results})
+      //console.log(response.data.results[0], "hello");
+      //console.log(this.state.users), "state ";
+      return response.data.results
   })
   .catch(function (error) {
     console.log(error);
   });
   }
   render() {
+    const { user, show, users } = this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -38,15 +46,32 @@ class App extends Component {
           <h1 className="App-title">TEst Welcome to React</h1>
         </header>
   <Carousel onSelect = {this.handleSelect}>
-      {this.state.users.map(user => 
-      <Carousel.Item onClick={this.handleClick}>
-        <img width={900} height={500} alt="900x500" src={user.picture.medium} />
-        <Carousel.Caption>
+      <Carousel.Item>
+      {users.map(user => 
+      <div key={user.name.first} onClick={this.handleClick.bind(this,user)}>
+        <img width={200} height={150} alt="900x500" src={user.picture.medium} />
           <h3>{user.name.first}</h3>
-        </Carousel.Caption>
-      </Carousel.Item>)}
-</Carousel>;
+      </div>)}
+      </Carousel.Item>
+  </Carousel>;
+  {user && show && <Modal show={show} onHide={this.handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>{user.name.first}</Modal.Title>
+    </Modal.Header>
 
+    <Modal.Body>{Object.keys(user).map((key) => {
+      const value = user[key]
+      return <div key={key}>
+        `${key}: ${JSON.stringify(value)}`
+      </div>
+    })}</Modal.Body>
+    <Modal.Body>{user.name.first}</Modal.Body>
+    
+
+    <Modal.Footer>
+      <Button onClick={this.handleClose}>Close</Button>
+    </Modal.Footer>
+  </Modal>}
       </div>
     );
   }
